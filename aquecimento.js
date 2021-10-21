692102d0d0f60799b78b21f6427311ae88061b81
Tavares304 <tavares.he@yahoo.com>
2021-10-21 00:43:31 -0300

    refatorando as variaveis

--- aquecimento.js
+++ aquecimento.js
@@ -1,24 +1,17 @@
-let nome;
-let produto;
-let preco;
-let desconto;
+let nome = "Henriqe Tavares de Lima";
+let produto  = "ThinkPad t470";
+let preco = 8000;
+let desconto = 5;
 
-nome = "Henriqe Tavares de Lima";
-produto  = "ThinkPad t470";
-preco = 8000;
-desconto = 5;
-
-console.log("Oi " + nome);
+console.log("Oi " + nome);  // eu poderia usr console.log(`Oi ${nome}`); mas nao esta funcionando no console do vscode usando o node
 console.log("Você comprou " + produto);
 console.log("Custou " + preco);
 console.log("Desconto de "+ desconto +"%.");
 
-let descontoCalculado;
-descontoCalculado = (preco * desconto) / 100;
+let descontoCalculado = (preco * desconto) / 100;
 
 console.log("Desconto calculado: " + descontoCalculado);
 
-let precoFinal;
-precoFinal = preco - descontoCalculado;
+let precoFinal = preco - descontoCalculado;
 
 console.log("Proço final: " + precoFinal);
