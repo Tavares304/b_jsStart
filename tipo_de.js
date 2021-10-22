88fad7c927fd73319dc11a7e2e4ac47663c4294d
Tavares304 <tavares.he@yahoo.com>
2021-10-21 23:40:37 -0300

    testando tipos

---
+++ tipo_de.js
@@ -0,0 +1,21 @@
+let texto = "Javascript Start";
+let numero = 300;
+let emAberto;
+
+let textoNovo = parseInt(texto);
+console.log("Valor do textoNovo: " + textoNovo);
+
+let numeroNovo = numero.toString();
+console.log("Valor do numeroNovo: " + numeroNovo);
+
+let a = "10.5";
+let b = "20.5";
+
+let somaAB = a + b;
+console.log("Valor do somaAB: " + somaAB);
+
+emAberto = parseInt(a) + parseInt(b);
+
+let npf = "30.45";
+let pontoFluante = parseFloat(npf);
+console.log("Valor do pontoFluante: " + pontoFluante);
