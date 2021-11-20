759e43c296b094d80918d2f41045e6067bd9b9be
Tavares304 <tavares.he@yahoo.com>
2021-11-19 21:20:31 -0300

    continuacao leitura

---
+++ case_sensitive.js
@@ -0,0 +1,7 @@
+let texto  = "Javascript Start";
+let Texto  = "Chiforinfola";
+let textO  = "Just Start";
+
+console.log(texto);
+console.log(Texto);
+console.log(textO);
---
+++ parse.js
@@ -0,0 +1,21 @@
+let texto  = "Javascript Start";
+let numero = 300;
+let emAberto;
+
+let textoNovo = parseInt(texto);
+console.log("valor de texto novo: " + textoNovo); // Mais uma vez aqui eu poderia ter usado interpolacao como no livro mas nao esta funcionando no meu vscode preciso descobrir porque.
+
+let numeroNovo = nemro.toString();
+console.log("Valor de NumeroNovo: " + numeroNovo);
+
+let a = "10.5";
+let b = "20.5";
+let junta = a + b;
+console.log("Valor de junta: " + junta);
+
+emAberto = parseInt(a) + parseInt(b);
+console.log("Valor de emAberto: " + emAberto);
+
+let npf = "30.45";
+let pontoFlutuante = parseFloat(npf);
+console.log("Valor de pontoFlutuante: " + pontoFlutuante);
